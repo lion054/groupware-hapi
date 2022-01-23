c502b33920e7ce86e0597909725474e54f1b58df
lion054 <lionel.siduna@gmail.com>
2022-01-23 05:57:48 -0700

    Change something

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,14 @@
 
 require("dotenv").config();
 
-const runCompanySeeder = require("./CompanySeeder");
-const runUserSeeder = require("./UserSeeder");
+const CompanySeeder = require("./CompanySeeder");
+const UserSeeder = require("./UserSeeder");
 
-runCompanySeeder();
-runUserSeeder();
+async function run() {
+  console.log("company seeder");
+  await CompanySeeder();
+  console.log("user seeder");
+  await UserSeeder();
+}
+
+run();
